3a09bfd0a6d5b07947041045a7a7c7f6e57646ac
bhavin-tops <125648209+bhavin-tops@users.noreply.github.com>
2023-04-25 20:49:27 +0530

    Delete with out val & with parameter.py

--- assignment/with out val & with parameter.py
+++
@@ -1,9 +0,0 @@
-#functiion: w/o return val & with parameter
-
-def display(a,b):
-    print("value of A:",a)
-    print("value of B:",b)
-    print("addition:",a+b)
-    
-#calling function
-    display(a,b)
