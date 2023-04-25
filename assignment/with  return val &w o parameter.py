d3b901865d3512ed7ce53e75f79e1052a2880d35
bhavin-tops <125648209+bhavin-tops@users.noreply.github.com>
2023-04-25 20:49:36 +0530

    Delete with  return val &w o parameter.py

--- assignment/with  return val &w o parameter.py
+++
@@ -1,7 +0,0 @@
-#function : wiyh return val /w/o parameter
-
-def add():
-    n1=int(input("enter the number 1:"))
-    n2=int(input("enter the number 2:"))
-    return n1+n2
-print("addition:",add())
