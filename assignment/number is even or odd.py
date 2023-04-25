f9ad8dd8d98a3076dd185839073d39b31a38dcca
bhavin-tops <125648209+bhavin-tops@users.noreply.github.com>
2023-04-25 20:52:23 +0530

    Delete number is even or odd.py

--- assignment/number is even or odd.py
+++
@@ -1,8 +0,0 @@
-# write a python program to find whether a given number is even or odd,print out appropriate massege to the user
-
-number=int(input("enter the number:"))
-
-if (number%2==0):
-    print("number is even")
-else:
-    print("number is odd")
